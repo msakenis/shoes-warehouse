cce9b58ae99b71a782d047cca8a79f707c0e313a
MindaugasSak <m.sakenis@gmail.com>
2021-01-12 22:01:03 +0200

    notifi for a success add and some input validation

--- src/pages/Products/CreateProduct/CreateProduct.jsx
+++ src/pages/Products/CreateProduct/CreateProduct.jsx
@@ -9,6 +9,7 @@ import {
   Button,
   Radio,
   RadioGroup,
+  useToast,
 } from '@chakra-ui/react';
 
 function createRandomEANNumber() {
@@ -23,7 +24,8 @@ function addProduct(
   color,
   weight,
   productStatus,
-  currentProducts
+  currentProducts,
+  toast
 ) {
   e.preventDefault();
   const id = currentProducts.sort((a, b) => a.id - b.id).slice(-1)[0].id + 1; //sorts by id to get highest id number and +1 to have unique id
@@ -32,13 +34,21 @@ function addProduct(
     id,
     name,
     type,
-    ean: EANnumber,
+    ean: +EANnumber,
     color,
     weight: +weight,
     active: productStatus,
   });
-  console.log(currentProducts);
-  localStorage.setItem('products', JSON.stringify(currentProducts));
+
+  localStorage.setItem('products', JSON.stringify(currentProducts)); // rewrites fake db to new products
+  toast({
+    title: 'Product created successfully!',
+
+    status: 'success',
+    duration: 9000,
+    isClosable: true,
+    position: 'top',
+  });
 }
 
 function CreateProduct() {
@@ -49,6 +59,7 @@ function CreateProduct() {
   const [weight, setWeight] = useState(0);
   const [productStatus, setProductStatus] = useState('true');
   const currentProducts = JSON.parse(localStorage.getItem('products'));
+  const toast = useToast();
 
   return (
     <div>
@@ -66,7 +77,8 @@ function CreateProduct() {
               color,
               weight,
               productStatus,
-              currentProducts
+              currentProducts,
+              toast
             );
           }}
         >
@@ -77,6 +89,7 @@ function CreateProduct() {
                 type="text"
                 onChange={(e) => setName(e.target.value)}
                 placeholder="Name"
+                maxLength="50"
               />
               <FormHelperText>e.g. "Adidas"</FormHelperText>
             </FormControl>
@@ -87,6 +100,7 @@ function CreateProduct() {
                 type="text"
                 onChange={(e) => setType(e.target.value)}
                 placeholder="Type"
+                maxLength="50"
               />
               <FormHelperText>e.g. "Sneakers" </FormHelperText>
             </FormControl>
@@ -96,10 +110,13 @@ function CreateProduct() {
             <FormControl id="ean" isRequired>
               <FormLabel>EAN Code</FormLabel>
               <Input
-                type="number"
-                onChange={(e) => setEANnumber(Number(e.target.value))}
+                type="text"
+                onChange={(e) => setEANnumber(e.target.value)}
                 value={EANnumber}
                 placeholder="Color"
+                minLength="13"
+                maxLength="13"
+                pattern="^[0-9]*$"
               />
               <FormHelperText>
                 This is randomly generated EAN code
@@ -112,16 +129,19 @@ function CreateProduct() {
                 type="text"
                 onChange={(e) => setColor(e.target.value)}
                 placeholder="Color"
+                maxLength="50"
               />
               <FormHelperText>e.g. "Red"</FormHelperText>
             </FormControl>
 
-            <FormControl id="weigth" isRequired>
-              <FormLabel>Weigth (g)</FormLabel>
+            <FormControl id="weight" isRequired>
+              <FormLabel>Weight (g)</FormLabel>
               <Input
-                type="number"
+                type="text"
                 onChange={(e) => setWeight(e.target.value)}
                 placeholder="Grams"
+                pattern="^[0-9]*$"
+                maxLength="7"
               />
               <FormHelperText>weight in grams. E.g 1000</FormHelperText>
             </FormControl>
