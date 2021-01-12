e5f53342f49854129fbbd48277a424f47e27d682
MindaugasSak <m.sakenis@gmail.com>
2021-01-12 19:11:41 +0200

    logo pushes to default route

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import logoImg from '../../assets/images/talechLogo.png';
-import { Link as ReactLink } from 'react-router-dom';
+import { Link as ReactLink, useHistory } from 'react-router-dom';
 import { Stack, Image, Link, Box } from '@chakra-ui/react';
 
 function NavBar() {
+  const history = useHistory();
   return (
     <Box borderBottom="2px" borderColor="gray.200">
       <Stack
@@ -13,7 +14,14 @@ function NavBar() {
         align="center"
         m="auto"
       >
-        <Image src={logoImg} p="4" width="200px" alt="logo" />
+        <Image
+          src={logoImg}
+          p="4"
+          width="200px"
+          alt="logo"
+          cursor="pointer"
+          onClick={() => history.push('/products')}
+        />
         <Stack direction="row" color="gray.500">
           <Link as={ReactLink} mr="4" to="/products">
             Products
