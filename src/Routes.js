a904fed13b84d6d5488a90471836e46b58151aed
MindaugasSak <m.sakenis@gmail.com>
2021-01-12 09:10:22 +0200

    chakra css framework added

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react';
 
 const Applazy = lazy(() => import('./pages/App'));
 
 function Routes() {
   return (
     <Router>
-      <Suspense fallback={'Loading...'}>
+      <Suspense fallback={<Spinner size="xl" />}>
         <Switch>
           <Route exact path="/" component={Applazy} />
         </Switch>
